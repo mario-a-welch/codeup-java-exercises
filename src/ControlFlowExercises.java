307f53c458a4951125dd0179b56524342d7f6068
mario welch <mario.welch01@gmail.com>
2021-12-16 13:34:33 -0600

    while and do while 0 - 100

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,16 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+//1. Loop Basics - a. While
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+//1. Loop Basics - b. Do while
+        int num = 0;
+        do {
+            System.out.println(num);
+            num += 2;
+        } while (num <= 100);
+    }
+}
