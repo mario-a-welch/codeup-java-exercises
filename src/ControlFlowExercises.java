5f9561e43215e9618f275aaa378977345e54f58a
mario welch <mario.welch01@gmail.com>
2021-12-16 14:59:23 -0600

    FizzBuzz exercise complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -43,11 +43,22 @@ public class ControlFlowExercises {
 //        }
 //Iterates from 100 - (-10) by 5
 
-        for(double num = 2; num <= 1000000; num = Math.pow(num, 2)) {
-            System.out.println(num);
-        }
+//        for(double num = 2; num <= 1000000; num = Math.pow(num, 2)) {
+//            System.out.println(num);
+//        }
 //Iterates from 2 to 1000000 by squares
 
+//2. Fizzbuzz
+        for(int i = 1; i <= 100; i++){
+            if (((i % 3) == 0) && ((i % 5) == 0)) // Multiple of 3 & 5
+                System.out.println("FizzBuzz");
+            else if ((i % 3) == 0) // Multiple of 3
+                System.out.println("Fizz");
+            else if ((i % 5) == 0) // Multiple of 5
+                System.out.println("Buzz");
+            else
+                System.out.println(i); // Not a multiple of 3 or 5
+        }
 
     }
 }
