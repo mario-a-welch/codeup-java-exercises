e757310d7d1c1682352ee320fd0f833b2a485b67
mario welch <mario.welch01@gmail.com>
2022-01-03 10:11:06 -0600

    updated exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import java.text.DecimalFormat;
+
 public class ControlFlowExercises {
     public static void main(String[] args){
 //1. Loop Basics - a. While
@@ -24,10 +28,10 @@ public class ControlFlowExercises {
 //Iterates from 100 - (-10) by 5
 
 // 3rd part of b. Do while
-//        double num = 2;
+//        int num = 2;
 //        do {
 //            System.out.println(num);
-//            num = Math.pow(num, 2);
+//            num = (int) Math.pow(num, 2);
 //        } while (num <= 1000000);
 //Iterates from two to a million by each line squared
 
@@ -43,22 +47,78 @@ public class ControlFlowExercises {
 //        }
 //Iterates from 100 - (-10) by 5
 
-//        for(double num = 2; num <= 1000000; num = Math.pow(num, 2)) {
+//        for(int num = 2; num <= 1000000; num = (int) Math.pow(num, 2)) {
 //            System.out.println(num);
 //        }
+
+//        for(long num = 2; num < 1000000; num *= num){
+//            System.out.println(num);
+//        }
+//Another way to solve
 //Iterates from 2 to 1000000 by squares
 
 //2. Fizzbuzz
-        for(int i = 1; i <= 100; i++){
-            if (((i % 3) == 0) && ((i % 5) == 0)) // Multiple of 3 & 5
-                System.out.println("FizzBuzz");
-            else if ((i % 3) == 0) // Multiple of 3
-                System.out.println("Fizz");
-            else if ((i % 5) == 0) // Multiple of 5
-                System.out.println("Buzz");
-            else
-                System.out.println(i); // Not a multiple of 3 or 5
+//        for(int i = 1; i <= 100; i++){
+//            if (((i % 3) == 0) && ((i % 5) == 0)) // Multiple of 3 & 5
+//                System.out.println("FizzBuzz");
+//            else if ((i % 3) == 0) // Multiple of 3
+//                System.out.println("Fizz");
+//            else if ((i % 5) == 0) // Multiple of 5
+//                System.out.println("Buzz");
+//            else
+//                System.out.println(i); // Not a multiple of 3 or 5
+//        }
+
+//3. Display a table of powers
+        Scanner scanner =  new Scanner(System.in);
+
+        DecimalFormat df = new DecimalFormat("0");
+
+        boolean userContinue = true;
+
+        while (userContinue) {
+            System.out.println("Enter a number");
+            int userInput = scanner.nextInt();
+
+            System.out.println("Here is your table!");
+            System.out.println();
+            System.out.println("number  | squared   | cubed");
+            System.out.println("------- | --------- | -----");
+            for(int num = 1; num <= userInput; num++){
+                System.out.println(df.format(Math.pow(num,1)) + "\t\t| " + df.format(Math.pow(num, 2)) + " \t\t| " + df.format(Math.pow(num, 3)));
+            }
+            System.out.println("Do you want to enter a new number");
+            userContinue = scanner.nextBoolean();
         }
+
+//4. Convert number to letter grades
+
+        String continueChoice = "y";
+        while (continueChoice.equalsIgnoreCase("y"))
+        {
+            //get user input
+            System.out.print("Enter Numeric Grade:\t\t");
+            double score = scanner.nextDouble();
+
+            //converts users numeric grade into letter grade
+            char userGrade = 'F';
+            if(score >= 88)
+                userGrade = 'A';
+            else if(score >= 80)
+                userGrade = 'B';
+            else if (score >= 67)
+                userGrade = 'C';
+            else if (score >= 60)
+                userGrade = 'D';
+
+            // display conversion result
+            System.out.println("Equivalent Letter Grade:\t" + userGrade);
+
+            //Checks to see if user wants to continue
+            System.out.print("\nContinue? (y/n):\t\t");
+            continueChoice = scanner.next();
+            System.out.println();
+        }//end while loop
 
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,99 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Java is fun!";
+        System.out.println(myString);
+
+//        Will receive an error
+//        myString = 'a';
+//        System.out.println(myString);
+
+
+//        Will receive an error
+//        myString = 3.14149;
+//        System.out.println(myString);
+
+//When printing a variable with no value, you will get back an error stating variable has not been initialized
+//        long myNumber;
+//        System.out.println(myNumber);
+
+//When assigning a decimal value to long, you will get back incompatible types error: possible lossy conversion from double to long
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+// Adding an L at the end of 123 does not return an error, signifies long
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+//Will compile when run, result in no errors
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+//Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+// 3.14 must be declared as a double or a float because it is a decimal value
+
+
+//Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//Will result in an error, incompatible types: possible lossy conversion from double to float
+//        Solve#1
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//        Solve#2
+//        float myNumber = 3.14F
+//        Solve#3
+//        float myNumber = (float)3.14
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+// Outputs 5, then 6
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//Outputs 6, then 6
+
+//Try to create a variable named class. What happens?
+//        class codeup = 'c';
+//Creating a variable of class will result in an error
+
+
+//Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//Error: java.lang.String incompatible with java.lang.Integer at java.base/java.lang.ClassCastException.
+
+//        int three = (int) "three";
+//Error: incompatible types: java.lang.String cannot be converted to int
+
+//        int x = 4;
+//        x = x + 5;
+//        x+=5;  //Shorthand assignment
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y*=x; //Shorthand assignment
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+        x/=y; //Shorthand assignment
+//        y = y - x;
+        y-=x; //Shorthand assignment
+        System.out.println(x);
+        System.out.println(y);
+
+
     }
 }
