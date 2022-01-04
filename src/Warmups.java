27da5ad808fd193344609098667390e88b479ede
mario welch <mario.welch01@gmail.com>
2022-01-04 09:19:21 -0600

    updated method placement

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Warmups {
+
+    public static String startDay(){
+        return "Brain loading... Done!";
+    }
+
     public static void main(String[] args) {
         String myString = "Java is statically typed, while Javascript is dynamically typed.";
         System.out.println(myString);
@@ -13,9 +18,5 @@ public class Warmups {
         System.out.println("Thanks for telling me you ate: " + userBreakfast);
 
         System.out.println(startDay());
-    }
-
-    public static String startDay(){
-        return "Brain loading... Done!";
     }
 }
