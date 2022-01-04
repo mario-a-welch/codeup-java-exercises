277ca8385db2f41b51ad252ce34ca89933dfb059
mario welch <mario.welch01@gmail.com>
2022-01-04 14:31:13 -0600

    refactored factorial method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -12,6 +12,22 @@ public class MethodsExercises {
         return num1 * num2;
     }
 
+    public static void noMultiplyOperator(int num1, int num2){
+        int result = 0;
+        for (int i = 1; i <= num1; i++){
+            result += num2;
+        }
+        System.out.println(result);
+    }
+
+    public static int multiplyWithRecursion(int num1, int num2){
+        if(num2 == 0){
+            return 0;
+        }else{
+            return num1 + multiplyWithRecursion(num1, num2 -1);
+        }
+    }
+
     public static int divideNums(int num1, int num2){
         return num1/num2;
     }
@@ -36,17 +52,17 @@ public class MethodsExercises {
         Scanner sc = new Scanner(System.in);
         int userInput;
         String userContinue;
-        long factor = 1;
-        String mid = "1";
 
         do{
+            long factor = 1;
+            String mid = "1";
            userInput = getInteger(1, 10);
            for(int i = 1; i <= userInput; i++){
                if(i>1){
                    mid += "x" + i;
                }
                factor *= i;
-               System.out.printf("%2d! = %-40s = %d%n", i, mid, factor);
+               System.out.printf("%d! = %-25s = %d%n", i, mid, factor);
            }
            System.out.println("Do you want to continue? (y/n)");
            userContinue = sc.nextLine();
@@ -80,6 +96,8 @@ public class MethodsExercises {
         System.out.println(addNums(7, 7));  //14
         System.out.println(subtractNums(9, 8)); //1
         System.out.println(multiplyNums(5, 9));  //45
+        noMultiplyOperator(5, 5); //25
+        System.out.println(multiplyWithRecursion(2, 3)); //6
         System.out.println(divideNums(56, 8)); //7
         System.out.println(remainderOfNums(8, 2)); //0
 
