3021e7d946d48a7d37bbb3fbfefa43ef252cc7b3
mario welch <mario.welch01@gmail.com>
2022-01-10 09:25:41 -0600

    added notes after walkthrough

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,11 +1,11 @@
 package shapes;
 
 public class Rectangle {
-    protected Double length;
-    protected Double width;
+    protected double length;
+    protected double width;
 
     //Constructor that accepts two numbers for length and width
-    public Rectangle(Double length, Double width) {
+    public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -13,6 +13,9 @@ public class Square extends Rectangle{
     public double getArea(){
         System.out.println("Override Rectangle getArea()");
         return this.side * this.side;
+        //return super.length * super.length;
+        //will not have to use private double side and this.side
+        //can also use just length and width variables since they are protected but will not work if they are set to private in initial class
     }
 
     public double getPerimeter(){
